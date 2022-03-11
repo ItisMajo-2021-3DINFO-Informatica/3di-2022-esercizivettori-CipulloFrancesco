b4d8c9d7e727b854ed3b8483fbaf12dd181325bc
CipulloFrancesco <98150895+CipulloFrancesco@users.noreply.github.com>
2022-03-11 10:26:22 +0100

    ConsegnaVettori1

---
+++ CipulloVettori1/CipulloVettori1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CipulloVettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            int c = 0;
+            
+            Random generatore = new Random();
+            
+
+            for(c=0;c<dimensione;c++)
+            {
+                int numerocasuale = generatore.Next(1, 100);
+                Console.WriteLine($"{numerocasuale}");
+            }
+
+            
+        }
+    }
+}
